82b46ee5239c814fbbfa5dc7be140e1ab28aa412
jeancarlos94 <jeangplay@gmail.com>
2021-08-15 15:38:34 -0300

    Start

--- src/main/java/com/jean/PersonAPI/service/PersonService.java
+++ src/main/java/com/jean/PersonAPI/service/PersonService.java
@@ -6,6 +6,7 @@ import com.jean.PersonAPI.entity.Person;
 import com.jean.PersonAPI.exception.PersonNotFoundException;
 import com.jean.PersonAPI.mapper.PersonMapper;
 import com.jean.PersonAPI.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +15,10 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__ (@Autowired))
 public class PersonService {
     private final PersonRepository personRepository;
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
