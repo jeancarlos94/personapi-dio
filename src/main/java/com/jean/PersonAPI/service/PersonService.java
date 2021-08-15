ddc78f27c64bf076ac003af5c3561f49d1fc2a5f
jeancarlos94 <jeangplay@gmail.com>
2021-08-15 13:27:29 -0300

    Delete Person ById

--- src/main/java/com/jean/PersonAPI/service/PersonService.java
+++ src/main/java/com/jean/PersonAPI/service/PersonService.java
@@ -43,11 +43,18 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-        Person person = personRepository.findById(id)
-                .orElseThrow(() -> new PersonNotFoundException(id));
-
+        Person person = verifyIfExists(id);
         return personMapper.toDto(person);
     }
 
+    public Person verifyIfExists(Long id) throws PersonNotFoundException{
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
+    }
 
+
+    public void delete(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+        personRepository.deleteById(id);
+    }
 }
